c783a86324170ec7f04ac91e11969c8263cc18b7
zhangfaguo <zhangfaguo98@163.com>
2020-05-25 16:31:13 +0800

    add gitigron

--- XiaShu/Form1.cs
+++ XiaShu/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -69,6 +70,21 @@ namespace XiaShu
 
         }
 
+        private string GetString1(string url)
+        {
+            var request = WebRequest.Create(url);
+            var resp = request.GetResponse();
+            var stream = resp.GetResponseStream();
+            using (var gstream = new GZipStream(stream, CompressionMode.Decompress))
+            {
+                using (StreamReader reader = new StreamReader(stream, Encoding.GetEncoding("gbk")))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+
+        }
+
         private void SaveLog(string html)
         {
             using (var s = new StreamWriter("d:\\1.txt"))
@@ -79,7 +95,7 @@ namespace XiaShu
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            var html = await GetString("https://www.52bqg.com/book_91371/");
+            var html = await GetString("https://www.52bqg.com/book_91371/https://www.52bqg.com/book_91371/");
 
             var matchcs = Regex.Matches(html, "<dd><a href=['\"](?<url>.*?)['\"].*?</dd>");
 
@@ -104,6 +120,50 @@ namespace XiaShu
                 sb.AppendLine(content);
             }
             File.AppendAllText("d:\\都市最强修真学生.txt", sb.ToString());
+            MessageBox.Show("Ok");
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            var html1 = await GetString("https://www.236n.cn/7_7363/");
+
+            var matchcs = Regex.Matches(html1, "<dd><a href=['\"](?<url>.*?)['\"].*?</dd>");
+
+            var dict = new Dictionary<int, string>();
+            var i = 0;
+            foreach (Match m in matchcs)
+            {
+                var url = m.Groups["url"].Value;
+                dict.Add(i++, $"https://www.236n.cn/{url}");
+            }
+            var list = dict.Select(t => new { t.Key, t.Value }).Skip(9).ToList();
+
+
+
+            Dictionary<int, string> st = new Dictionary<int, string>();
+
+           list.AsParallel().ForAll((s) =>
+           {
+               var d = s;
+               var url = s.Value;
+               var html = GetString1(url);
+               var tMathc = Regex.Match(html, "<title>(?<t>.*?)_都市全能奶爸_笔趣阁</title>");
+               var contentMatch = Regex.Match(html, "<div id=\"content\">(?<c>[\\w\\W]+?)</div>");
+               var sb = new StringBuilder();
+               sb.AppendLine(tMathc.Groups["t"].Value);
+               var content = contentMatch.Groups["c"].Value;
+               content = content.Replace("\"", "").Replace("&nbsp;", "").Replace("<br />", "").Replace("顶点手机站 m.236n.cn", "");
+               sb.AppendLine(content);
+               st.Add(d.Key, sb.ToString());
+           });
+
+
+            var con = new StringBuilder();
+            foreach (var t in st.AsEnumerable().OrderBy(t => t.Key))
+            {
+                con.AppendLine(t.Value);
+            }
+            File.AppendAllText("d:\\都市全能奶爸.txt", con.ToString());
             MessageBox.Show("Ok");
         }
     }
